015cc90659e6a04a88d4d7419ff3305ad0d26031
JDuPreez <medium.com/@recursiveenigma>
2018-04-06 11:50:05 +0200

    Untested version of Matarisvan.Performance.get ...

--- Matarisvan/Logs.fs
+++ Matarisvan/Logs.fs
@@ -6,7 +6,7 @@ open Matarisvan.Performance
 
 type LogItem = CsvProvider<"C:\Projects\Matarisvan\Matarisvan\AppLogs.csv", Schema=",date option,,,,,,,,,,,">
 
-let load (file : string) = 
+let load (file : string)  : LogItem.Row seq = 
   let log = LogItem.Load(file)
   log.Rows
 
@@ -16,31 +16,33 @@ let timespan (logItems : LogItem.Row seq) =
   let ends = Seq.maxBy<LogItem.Row, DateTime option> (fun li -> li.Date) logItems
   Seq.head logItems, (ends.Date.Value - begins.Date.Value).Milliseconds
 
-let convert (arg: LogItem.Row * int) =
+let convert (arg: (LogItem.Row * int)) =
   let logItem, responseTime = arg
   let callingMethod = logItem.CallingMethod
   performanceItem logItem.Date logItem.Level callingMethod responseTime 0 // date logLevel resource responseTime responseLength
 
 let getBy pred src = Seq.filter<LogItem.Row> pred src
-                      |> Seq.groupBy  (fun li -> li.CorrelationId)
-                      |> Seq.map      (fun li -> snd li
-                                                  |> Seq.toList
-                                                  |> timespan
-                                                  |> convert)
-                      |> Seq.filter   (fun x -> x.ResponseTime > 100) // Only those taking longer than 1 second
+                      |> Seq.groupBy  (fun li       -> li.CorrelationId)
+                      |> Seq.map      (fun li       -> snd li
+                                                        |> Seq.toList
+                                                        |> timespan
+                                                        |> convert)
+                      |> Seq.filter   (fun perfItem -> perfItem.ResponseTime > 100) // Only those taking longer than 1 second
 
 let getByCallingMethod logFile (callingMethod: string) =
   getBy (fun li -> li.CallingMethod = callingMethod) (load logFile)
 
 // Function "get" will draw a chart for each CallingMethod. So 1st group by CallingMethod,
 // and then group by correlation ID (almost like a nested "getByCallingMethod")
-// TODO: Fix this???
-(*let get logFile =
-  Seq.groupBy (fun li -> li.CallingMethod) (load logFile)
-  |> Seq.map (fun li -> fst li, snd li
-                                |> Seq.groupBy (fun li -> li.CorrelationId)
-                                |> Seq.map (fun li -> fst li, (snd li
-                                                              |> Seq.toList
-                                                              |> timespan
-                                                              |> convert))
-                                |> Seq.filter (fun x -> x.ResponseTime > 100))*)
+// TODO: Test this!!!
+let get logFile =
+  load logFile
+  |> Seq.groupBy  (fun li -> li.CallingMethod)
+  |> Seq.map      (fun li -> fst li, (snd li
+                                       |> Seq.groupBy  (fun li       -> li.CorrelationId)
+                                       |> Seq.map      (fun (_, logItms) ->  logItms
+                                                                             |> Seq.toList
+                                                                             |> timespan
+                                                                             |> convert)))
+  |> Seq.map      (fun (cm, perfItems) -> cm,  perfItems
+                                               |> Seq.filter (fun pi -> pi.ResponseTime > 100))
--- Matarisvan/Performance.fs
+++ Matarisvan/Performance.fs
@@ -5,5 +5,5 @@ open FSharp.Data
 
 type PerformanceItem = CsvProvider<"C:\Projects\Matarisvan\Matarisvan\PerformanceItem.csv", " | ", Schema="date option,,,,,,,">
 
-let performanceItem date logLevel resource responseTime responseLength = 
+let performanceItem date logLevel resource responseTime responseLength : PerformanceItem.Row = 
     PerformanceItem.Row(date, null, null, null, null, null, logLevel, null, null, null, null, null, resource, null, null, responseTime, null, null, responseLength)
